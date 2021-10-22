e32f892881364018d71ba9ab9ad4376818a30875
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2021-10-22 11:10:55 -0500

    Sticky header
    Posicionamiento de sticky header para las tres paginas. Limpieza de codigo.

--- Trabajo practico HTML, CSS, Javascript/app.js
+++ Trabajo practico HTML, CSS, Javascript/app.js
@@ -1,17 +1,14 @@
+
 function clickCounter() {
     if(typeof(Storage) !== "undefined") {
       if (sessionStorage.clickcount) {
-          if (sessionStorage.clickcount == 1){
-            sessionStorage.clickcount = Number(sessionStorage.clickcount)-1;
-          }
-          else{
-            sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
-          }
+          
+        sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+        displayMenu();
+
       }else {
         sessionStorage.clickcount = 1;
-      }
-        document.getElementById("menu").innerHTML = "Galeria";
-        displayMenu();
+      }  
     } else {
       document.getElementById("menu").innerHTML = "Sorry, your browser does not support web storage...";
     }
@@ -21,20 +18,12 @@ function clickCounter() {
 function displayMenu(){
 
     if(sessionStorage.clickcount % 2 != 0){
-        //document.getElementById("myContainer").style.gridTemplateRows = 
-        //"1fr 4fr 10fr 5fr";
-        //document.getElementById("imagenMain").style.gridRow = 3;
-        //document.getElementById("aside").style.gridRow = 3;
-        //document.getElementById("menu").style.gridRow = 2;
+        
         document.getElementById("menu").style.opacity = 1;
-        //document.getElementById("footer").style.gridRow = 4;
+        
     }
     else{
         
-        document.getElementById("myContainer").style.gridTemplateRows = 
-        "1fr 10fr 5fr";
-        document.getElementById("imagenMain").style.gridRow = 2;
-        document.getElementById("aside").style.gridRow = 2;
         document.getElementById("menu").style.opacity = 0;
     }
 }
@@ -42,5 +31,5 @@ function displayMenu(){
 function onMouseOver(element, color){
 
         element.style.color = color;
-}
 
+}
